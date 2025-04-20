2c255766a8396bb6f40347df0bbdaa1acdfeea70
micymike <mosesmichael878@gmail.com>
2025-04-21 00:55:44 +0300

    added host

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   server: {
-    port: 5173,
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
   },
   plugins: [react()],
   optimizeDeps: {
